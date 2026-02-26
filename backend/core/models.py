83b4d9f543fba08128d5330b4ddb93871fced775
Monika kumari <kmonika166@gmail.com>
2026-02-26 22:54:01 +0530

    connected add subject to the database

--- backend/core/models.py
+++ backend/core/models.py
@@ -11,3 +11,21 @@ class Faculty(models.Model):
 
     def __str__(self):
         return f"{self.full_name} ({self.designation})"
+class Subject(models.Model):
+    subject_code = models.CharField(max_length=20)
+    subject_name = models.CharField(max_length=200)
+    semester = models.IntegerField()
+    branch = models.CharField(max_length=100)
+    session = models.CharField(max_length=20)
+    batch = models.CharField(max_length=20)
+
+    created_by = models.ForeignKey(
+        Faculty,
+        on_delete=models.CASCADE,
+        related_name="created_subjects"
+    )
+
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.subject_code} - {self.subject_name}"
--- backend/core/urls.py
+++ backend/core/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Auth
     path('register/', views.register_faculty),
     path('login/', views.login_faculty),
+
+    # Subjects
+    path('add-subject/', views.add_subject),
+    path('get-hod-subjects/', views.get_hod_subjects),
+    path("update-subject/<int:subject_id>/", views.update_subject),
 ]
