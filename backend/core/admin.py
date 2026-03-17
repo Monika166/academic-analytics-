63e000fa955304fd428b69401a8e1a50d382e324
Monika kumari <kmonika166@gmail.com>
2026-03-17 22:32:55 +0530

    changed the ui of grid in professor dashboard

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import Faculty
+from .models import Faculty,Subject,Student,CourseOutcome
 
 admin.site.register(Faculty)
+admin.site.register(Subject)
+admin.site.register(Student)
+admin.site.register(CourseOutcome)
